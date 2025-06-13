669a21fbfebfc55312f99dc220a07ddff15e91cd
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-13 18:51:30 +0530

    compair 2 nos

---
+++ a2q11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter value of a & b=");
+    scanf("%d%d",&a,&b);
+    if(a==b)
+        printf("both are equal");
+    else if(b>a)
+        printf("b is greater than a");
+    else if(b<a)
+        printf("b is less than a");        
+}
