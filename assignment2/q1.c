73fdc3bb05f4663f3d41518bc2912927f82d0dd9
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-13 14:25:11 +0530

    positive negative

---
+++ assignment2/q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter no=");
+    scanf("%d",&n);
+    if(n>=0)
+    printf("\nno is positive");
+    else
+    printf("\n no is negative");
+}
