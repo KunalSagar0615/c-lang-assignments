a5a0a2ea6a2e0e04096eb11aee6aecaf1161677e
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-13 18:38:18 +0530

    vovel or not

--- a2q8.c
+++ a2q8.c
@@ -2,5 +2,15 @@
 void main()
 {
     char ch;
-    printf("\n enter lowercase char=")
+    printf("\n enter lowercase char=");
+    scanf("%s",&ch);
+    if(ch>=97 && ch<=122)
+    {
+    if(ch=='a' || ch == 'e' || ch== 'i'|| ch =='o' || ch=='u')
+    printf("given character is vovel");
+    else
+    printf("given character is consonant");
+    }
+    else
+    printf("you entered other than lowercase character");
 }
