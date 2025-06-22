3c49d285f7b83f93df7bd959240086066a78cee6
Kunal Ananda Sagar <kunalsagar0615@gmail.com>
2025-06-22 13:49:02 +0530

    table from 1 to n

--- a5q2.c
+++ a5q2.c
@@ -2,11 +2,11 @@
 void main()
 {
     int n,i,j;
-    printf("ente rnoumber=");
+    printf("enter number=");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        printf("\n \n table of %d \n",i);
+        printf("\n \n table of %d \n ",i);
         for(j=1;j<=10;j++)
         {
             printf("%d * %d = %d\n",i,j,(i*j));
